9d4c341c2931af70c9ca128cac0aba9f87bcf43d
motibegna <begna002@umn.edu>
2024-03-16 11:44:56 -0500

    Initial Commit

---
+++ SENG-5199-Assignment3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SENG-5199-Assignment3
+//
+//  Created by Moti Begna on 3/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SENG-5199-Assignment3/SENG_5199_Assignment3App.swift
@@ -0,0 +1,17 @@
+//
+//  SENG_5199_Assignment3App.swift
+//  SENG-5199-Assignment3
+//
+//  Created by Moti Begna on 3/16/24.
+//
+
+import SwiftUI
+
+@main
+struct SENG_5199_Assignment3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
