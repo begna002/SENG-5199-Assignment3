ad065541a11447ed5cd70acfc3ceef97770dd58a
motibegna <begna002@umn.edu>
2024-03-17 00:08:27 -0500

    Updating word list

--- SENG-5199-Assignment3/Strings.swift
+++ SENG-5199-Assignment3/Strings.swift
@@ -7,11 +7,11 @@
 
 import Foundation
 
-var foodWords = "Apple Bread Steak Pasta Salad Pizza Bacon Onion Lemon Candy Fruit Grill Toast Sushi Chees Prawn Muffi Burge Rice Peach"
-var dangerWords = "Risky Peril Toxic Blood Venom Flame Tiger Crash Storm Fears Alert Wound Blare Quake Siren Taint Alarm Grief Scare"
-var financeWods = "Asset Stock Bonds Trade Loans Debts Funds Value Taxes Yield Rates Price Richy Risky Taxed Costs Money Bonus Loans Bills"
+var foodWords = "Apple Bread Steak Pasta Salad Pizza Bacon Onion Lemon Candy Fruit Grill Toast Sushi Sushi Prawn Donut Mango Rice Peach"
+var dangerWords = "Risky Peril Toxic Blood Venom Flame Tiger Crash Storm Fears Alert Wound Blare Quake Siren Spoil Alarm Grief Scare"
+var financeWods = "Asset Stock Bonds Trade Loans Debts Funds Value Taxes Yield Rates Price Grant Risky Offer Costs Money Bonus Loans Bills"
 var geographyWords = "Coast Plain Delta River Ocean Hills Mount Plate Ridge Lakes Field Basin Dunes Swale Beach Tidal Fjord Flats Creek Cliffs"
-var writtingWords = "Write Novel Draft Words Inked Poems Typed Manus Sonet Typed Lines Rhyme Verse Quill Notes Texts Essay Script Story Prose"
+var writtingWords = "Write Novel Draft Words Inked Poems Typed Index Books Paper Lines Rhyme Verse Quill Notes Texts Essay Script Story Prose"
 
 var wordMap = [
     "Food": foodWords.components(separatedBy: " "),
